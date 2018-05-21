0034f4e5ef4a0e63ad84f63efc8e4624d86ab41e
崔晋瑜 <2510271615@qq.com>
2018-05-21 17:36:46 +0800

    Add files via upload

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,12 +1,29 @@
 let webpackMerge = require('webpack-merge');
 let commonConfig = require('./webpack.common');
+
 let path = require("path");
 
 module.exports = webpackMerge(commonConfig,{
         devtool:"eval-source-map",
+        rules:[
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                include: /src|packages/,
+                enforce: 'post',
+                use: [{
+                    loader: "istanbul-instrumenter-loader",
+                    options: {
+                        esModules: true
+                    },
+                }]
+            },
+        ],
         devServer: {
-        contentBase:path.resolve(__dirname,"../dist"),
+            contentBase:path.resolve(__dirname,"../dist"),
             historyApiFallback:true,
-            inline:true
+            inline:true,
+            hot:true,
+            compress:true
        }
 })
