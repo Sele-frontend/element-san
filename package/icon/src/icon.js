5b81ff13a27b4468c86a333e944e9586289d0856
unknown <email@xluos.com>
2018-05-23 23:23:17 +0800

    icon组件初步完成

--- package/icon/src/icon.js
+++ package/icon/src/icon.js
@@ -2,14 +2,14 @@
  * @Author: xu 
  * @Date: 2018-05-23 20:07:29 
  * @Last Modified by: xu
- * @Last Modified time: 2018-05-23 20:16:33
+ * @Last Modified time: 2018-05-23 22:51:27
  */
 
 import san, {DataTypes} from 'san';
 
 export default san.defineComponent({
-    template: `<i class="el-icon-"+name></i>`,
-    
+    template: `<i class="{{ 'el-icon-'+name }}"></i>`,
+
     initData() {
         
         return {
