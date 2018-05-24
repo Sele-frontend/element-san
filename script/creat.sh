e2ebefd8d49a44529b60579e443906b943552b11
unknown <email@xluos.com>
2018-05-24 10:30:57 +0800

    修改生成脚本

--- script/creat.sh
+++ script/creat.sh
@@ -3,22 +3,29 @@ cd package
 mkdir $1
 cd $1
 mkdir src
-touch $1.md
-echo "#$1">$1.md
+touch README.md
+echo "#$1">README.md
 touch index.js
 echo "import $1 from './src/$1.js';
 
-      export default $1;">index.js
+export default $1;">index.js
 cd src
 touch $1.js
 echo "import {defineComponent} from 'san';
-      export default defineComponent({
+export default defineComponent({
 
-          template:'',
-          initData() {
-              return {
-              }
-          }
+    template:'',
+    initData() {
+        return {
+        }
+    }
 
-      })" >$1.js
+})" >$1.js
 
+cd ../../../test/unit/specs/
+touch $1.spec.js
+
+cd ../../../theme
+touch $1.less
+
+echo "@import 'common/var';">$1.less
