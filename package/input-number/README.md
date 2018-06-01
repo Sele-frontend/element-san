7ba5fe7562fefff2026185b4598f0bd44a0c2be7
cuijinyu <2510271615@qq.com>
2018-06-01 09:49:16 +0800

    增加了input-number的高精度模式

--- package/input-number/README.md
+++ package/input-number/README.md
@@ -16,3 +16,6 @@ import elInputNumber from '../package/input-number/index.js';
 | size   | "small" "normal" "mini" "medium"| normal是正常大小 small是小 mini是最小 medium是适中   |
 | min  | type:number | 最小值 |
 | max |  type:number | 最大值 |
+| precisionFlag| boolean| 是否开启高精度模式|
+|precision| type:number| 精度|
+|step|type:number|步值|
--- package/input-number/src/input-number.js
+++ package/input-number/src/input-number.js
@@ -1,10 +1,37 @@
 import {defineComponent} from 'san';
 import elInput from '../../input/index';
+
+const getPrecision = function (value) {
+    if (value === undefined) {
+        return 0;
+    }
+    const valueString = value.toString();
+    const dotPosition = valueString.indexOf(".");
+    let precision = 0;
+    if (dotPosition !== -1) {
+        precision =  valueString.length - dotPosition - 1;
+    }
+    return precision;
+}
+
+const toPrecision = function (value, precision) {
+    return parseFloat(parseFloat(Number(value).toFixed(precision)));
+}
+
 export default defineComponent({
 
     template:`
         <div class="is-number-{{size}}">
-            <div class="el-input-number__decrease {{value==min?'disabled':''}}" on-click="sub(step)">
+            <div class="el-input-number__decrease 
+                        {{value==min?'disabled':''}}" 
+                        on-click="sub(step)" 
+                        s-if="!precisionMode">
+                <i class="el-icon-minus"></i>
+            </div>
+            <div class="el-input-number__decrease 
+                        {{value==min?'disabled':''}}" 
+                        on-click="subPrecision(step)" 
+                        s-if="precisionMode">
                 <i class="el-icon-minus"></i>
             </div>
             <el-input value="{=value=}" 
@@ -14,7 +41,16 @@ export default defineComponent({
                       on-focus="handleFocus" 
                       on-blur="handleBlur" 
                       on-input="handleInput"></el-input>
-            <div class="el-input-number__increase {{value==max?'disabled':''}}" on-click="add(step)">
+            <div class="el-input-number__increase 
+                       {{value==max?'disabled':''}}" 
+                       on-click="add(step)" 
+                       s-if="!precisionMode">
+                <i class="el-icon-plus"></i>
+            </div>
+            <div class="el-input-number__increase 
+                        {{value==max?'disabled':''}}" 
+                        on-click="addPrecision(step)" 
+                        s-if="precisionMode">
                 <i class="el-icon-plus"></i>
             </div>
         </div>
@@ -26,6 +62,8 @@ export default defineComponent({
             step:1,
             min:0,
             max:null,
+            precisionMode:false,
+            precision:2,    //  精度默认为2
         };
     },
     created () {
@@ -41,6 +79,12 @@ export default defineComponent({
                 }
             }
         });
+        if (this.data.get('precisionMode') === true) {
+            const precision = this.data.get("precision");
+            if (getPrecision(this.data.get('step')) > precision) {
+                console.warn('[ELEMENTSAN WARN][INPUT_NUMBER]:precision should not be less than the decimal places of step');
+            }
+        }
     },
     components:{
         'el-input':elInput
@@ -64,6 +108,24 @@ export default defineComponent({
             return;
         }
         this.data.set('value', value - n);
+    },
+    addPrecision (n) {
+        const value = this.data.get('value');
+        const precision = this.data.get('precision');
+        const precisionFactor = Math.pow(10, precision);
+        n = parseFloat(n);
+        let tempValue = (precisionFactor * n + precisionFactor * value) / precisionFactor;
+        tempValue = toPrecision(tempValue, precision);
+        this.data.set('value', tempValue);
+    },
+    subPrecision (n) {
+        const value = this.data.get('value');
+        const precision = this.data.get('precision');
+        const precisionFactor = Math.pow(10, precision);
+        n = parseFloat(n);
+        let tempValue = (precisionFactor * value - precisionFactor * n) / precisionFactor;
+        tempValue = toPrecision(tempValue, precision);
+        this.data.set('value', tempValue);
     },
     handleClick (e) {
         this.fire(e);
