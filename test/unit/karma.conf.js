45f5dd74ddaa25fcf9f7eae3339672e82d3c2666
cuijinyu <2510271615@qq.com>
2018-06-02 19:45:28 +0800

    修改了karma规则

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -28,12 +28,6 @@ module.exports = function(config) {
     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
