d8b35ab90ac62f1e77828dbbcd177ee4f16047b9
cuijinyu <2510271615@qq.com>
2018-06-16 00:08:37 +0800

    complete the disabled state of input-number component

--- package/input-number/src/input-number.js
+++ package/input-number/src/input-number.js
@@ -23,32 +23,37 @@ export default defineComponent({
     template:`
         <div class="is-number-{{size}} {{disabled?'el-input-number--disabled':''}}">
             <div class="el-input-number__decrease 
-                        {{value==min?'disabled':''}}" 
+                        {{value==min?'disabled':''}}
+                        {{disabled?'disabled':''}}" 
                         on-click="sub(step)" 
                         s-if="!precisionMode">
                 <i class="el-icon-minus"></i>
             </div>
             <div class="el-input-number__decrease 
-                        {{value==min?'disabled':''}}" 
+                        {{value==min?'disabled':''}}
+                        {{disabled?'disabled':''}}" 
                         on-click="subPrecision(step)" 
                         s-if="precisionMode">
                 <i class="el-icon-minus"></i>
             </div>
             <el-input value="{=value=}" 
-                      numberFlag="true" 
+                      numberFlag="true"
+                      disabled="{{disabled}}" 
                       size="{{size}}"
                       on-click="handleClick" 
                       on-focus="handleFocus" 
                       on-blur="handleBlur" 
                       on-input="handleInput"></el-input>
             <div class="el-input-number__increase 
-                       {{value==max?'disabled':''}}" 
+                       {{value==max?'disabled':''}}
+                       {{disabled?'disabled':''}}" 
                        on-click="add(step)" 
                        s-if="!precisionMode">
                 <i class="el-icon-plus"></i>
             </div>
             <div class="el-input-number__increase 
-                        {{value==max?'disabled':''}}" 
+                        {{value==max?'disabled':''}}
+                        {{disabled?'disabled':''}}" 
                         on-click="addPrecision(step)" 
                         s-if="precisionMode">
                 <i class="el-icon-plus"></i>
@@ -91,52 +96,64 @@ export default defineComponent({
         'el-input':elInput
     },
     add (n) {
-        const value = this.data.get('value');
-        n = parseInt(n);
-        if (this.data.get('max') !== null) {
-            if (value + n >= this.data.get('max')) {
-                this.data.set('value', this.data.get('max'));
-                return;
+        const disabledFlag = this.data.get('disabled');
+        if (!disabledFlag) {
+            const value = this.data.get('value');
+            n = parseInt(n);
+            if (this.data.get('max') !== null) {
+                if (value + n >= this.data.get('max')) {
+                    this.data.set('value', this.data.get('max'));
+                    return;
+                }
             }
+            this.data.set('value', value + n);
         }
-        this.data.set('value', value + n);
     },
     sub (n) {
-        const value = this.data.get('value');
-        n = parseInt(n);
-        if (value - n <= this.data.get('min')) {
-            this.data.set('value', this.data.get('min'));
-            return;
+        const disabledFlag = this.data.get('disabled');
+        if (!disabledFlag) {
+            const value = this.data.get('value');
+            n = parseInt(n);
+            if (value - n <= this.data.get('min')) {
+                this.data.set('value', this.data.get('min'));
+                return;
+            }
+            this.data.set('value', value - n);
         }
-        this.data.set('value', value - n);
     },
     addPrecision (n) {
-        const value = parseFloat(this.data.get('value'));
-        const precision = this.data.get('precision');
-        const precisionFactor = Math.pow(10, precision);
-        n = parseFloat(n);
-        let tempValue = (precisionFactor * n + precisionFactor * value) / precisionFactor;
-        tempValue = toPrecision(tempValue, precision).toString();
-        if (tempValue.length < parseInt(precision) + 2) {
-            for (let i = tempValue.length;i < parseInt(precision) + 2;i++) {
-                tempValue += '0';
+        const disabledFlag = this.data.get('disabled');
+        if (!disabledFlag) {
+            const value = parseFloat(this.data.get('value'));
+            const precision = this.data.get('precision');
+            const precisionFactor = Math.pow(10, precision);
+            n = parseFloat(n);
+            let tempValue = (precisionFactor * n + precisionFactor * value) / precisionFactor;
+            tempValue = toPrecision(tempValue, precision).toString();
+            if (tempValue.length < parseInt(precision) + 2) {
+                for (let i = tempValue.length;i < parseInt(precision) + 2;i++) {
+                    tempValue += '0';
+                }
             }
+            this.data.set('value', tempValue);
         }
-        this.data.set('value', tempValue);
     },
     subPrecision (n) {
-        const value = parseFloat(this.data.get('value'));
-        const precision = this.data.get('precision');
-        const precisionFactor = Math.pow(10, precision);
-        n = parseFloat(n);
-        let tempValue = (precisionFactor * value - precisionFactor * n) / precisionFactor;
-        tempValue = toPrecision(tempValue, precision).toString();
-        if (tempValue.length < parseInt(precision) + 2) {
-            for(let i = tempValue.length;i < parseInt(precision) + 2;i++){
-                tempValue += '0';
+        const disabledFlag = this.data.get('disabled');
+        if (!disabledFlag) {
+            const value = parseFloat(this.data.get('value'));
+            const precision = this.data.get('precision');
+            const precisionFactor = Math.pow(10, precision);
+            n = parseFloat(n);
+            let tempValue = (precisionFactor * value - precisionFactor * n) / precisionFactor;
+            tempValue = toPrecision(tempValue, precision).toString();
+            if (tempValue.length < parseInt(precision) + 2) {
+                for (let i = tempValue.length;i < parseInt(precision) + 2;i++) {
+                    tempValue += '0';
+                }
             }
+            this.data.set('value', tempValue);
         }
-        this.data.set('value', tempValue);
     },
     handleClick (e) {
         this.fire(e);
