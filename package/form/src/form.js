d2c55bc9975a50dc7e64375509f0e73b95bf14de
cuijinyu <2510271615@qq.com>
2018-06-24 16:27:25 +0800

    add some part of form and form-item and the css of transition

--- package/form/src/form.js
+++ package/form/src/form.js
@@ -1,10 +1,56 @@
 import {defineComponent} from 'san';
 export default defineComponent({
 
-    template:'',
+    template:`
+        <form class="el-form {{labelPosition?'el-form--label-'+labelPosition:''}}">
+            <slot></slot>
+        </form>
+    `,
     initData() {
         return {
+            formModel:undefined,
+            formItem:[],
+            labelWidth:'100px',
+            rules:{},
+            labelPosition:undefined,    //  标签位置，可能为三个值left、top或者为right
+            formStatus:'notValidated',
+            valid:true,  //  内容有效状态，默认为有效
+            size:'',    //  form表单大小
         }
-    }
+    },
+    attached () {
+        this.data.get('formItem').forEach( (ele) => {
+            if (this.data.get('labelWidth') !== undefined) {
+                ele.data.set('labelWidth', this.data.get('labelWidth'));
+            }
+        });
+    },
+    messages:{
+        'el.Form.addField':function (message) {
+            this.data.push('formItem',message.value);
+        },
+        'el.form.removeField':function (message) {
+            const index = this.data.get('formItem').indexOf(message.value);
+            this.data.slice('formItem',index,1);
+        }
+    },
+    resetField () {
+        this.data.get('formItem').forEach( (ele) => {
+            ele.reset();
+        })
+    },
+    async validate (callback) {
+        /**
+         *
+         * 表单验证
+         *
+         */
+        await this.data.get('formItem').forEach(ele=>{
+           ele.validate();
+        });
 
+        let valid = this.data.get('valid');
+
+        callback.call(this,valid);
+    }
 })
